687d77112b09430abdfdb3916a81171c036ee744
stenis <magset-8@student.ltu.se>
2023-09-13 13:31:58 +0200

    Start of the RPC and message system in place

--- kademlia/rpc.go
+++ kademlia/rpc.go
@@ -1,5 +1,18 @@
 package d7024e
 
+type RPC struct {
+	sender  Contact
+	msgType string
+	data    msgData
+}
+
+type msgData struct {
+	PING  string
+	STORE []byte
+	NODE  KademliaID
+	VALUE string
+}
+
 func PING() {
 
 }
