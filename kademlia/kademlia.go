ebeabea100b9b3fbe1e72e37dff48c0badbe3f35
nevbe <ellioth70@gmail.com>
2023-10-03 14:29:00 +0200

    added break

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -56,6 +56,7 @@ func (kademlia *Kademlia) initNode() {
 			if err == nil {
 				// Bootstrap node is alive, connect to it
 				kademlia.Network.Listen(bootstrapAddress) //TODO: Fix the listening function
+				break
 			} else {
 				// Error log
 			}
